e3a6846b6364d9789f2662ff6c91dd5d1675f0a3
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-04-08 15:39:47 -0500

    chore: prueba de spring

---
+++ src/main/java/co/edu/sena/eprueba/EPruebaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sena.eprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPruebaApplication.class, args);
+	}
+
+}

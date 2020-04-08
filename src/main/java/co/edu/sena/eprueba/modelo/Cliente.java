5876cb332c73710cd7d4e546b0d66859bcd1ecda
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-04-08 15:41:30 -0500

    chore: se crea la clase cliente

---
+++ src/main/java/co/edu/sena/eprueba/modelo/Cliente.java
@@ -0,0 +1,167 @@
+package co.edu.sena.eprueba.modelo;
+
+import java.util.Set;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "Cliente")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCli;
+	
+	@NotNull
+	@Size(max= 50)
+	@Column(name = "environment_number", length = 50, nullable = false)
+	private String nombreCli;
+	
+	@NotNull
+	@Size(max= 50)
+	private String apellidoCli;
+	
+	@NotNull
+	@Size(max= 50)
+	private int documentoCli;
+	
+	@NotNull
+	@Size(max= 100)
+	private String direccionCli;
+	
+	@NotNull
+	@Size(max= 30)
+	private int telefonoCli;
+	
+	@NotNull
+	@Size(max= 100)
+	private String emailCli;
+	
+	@OneToMany(mappedBy = "Cliente")
+	private Set<Reserva> reservas;
+	
+	
+	public Cliente() {
+		
+	}
+	
+	public Cliente(Long idCli, String nombreCli, String apellidoCli, int documentoCli, String direccionCli, int telefonoCli, String emailCli) {
+		this.idCli = idCli;
+		this.nombreCli = nombreCli;
+		this.apellidoCli = apellidoCli;
+		this.documentoCli = documentoCli;
+		this.direccionCli = direccionCli;
+		this.telefonoCli = telefonoCli;
+		this.emailCli = emailCli;
+	}
+
+	public Long getIdCli() {
+		return idCli;
+	}
+
+	public void setIdCli(Long idCli) {
+		this.idCli = idCli;
+	}
+
+	public String getNombreCli() {
+		return nombreCli;
+	}
+
+	public void setNombreCli(String nombreCli) {
+		this.nombreCli = nombreCli;
+	}
+
+	public String getApellidoCli() {
+		return apellidoCli;
+	}
+
+	public void setApellidoCli(String apellidoCli) {
+		this.apellidoCli = apellidoCli;
+	}
+
+	public int getDocumentoCli() {
+		return documentoCli;
+	}
+
+	public void setDocumentoCli(int documentoCli) {
+		this.documentoCli = documentoCli;
+	}
+
+	public String getDireccionCli() {
+		return direccionCli;
+	}
+
+	public void setDireccionCli(String direccionCli) {
+		this.direccionCli = direccionCli;
+	}
+
+	public int getTelefonoCli() {
+		return telefonoCli;
+	}
+
+	public void setTelefonoCli(int telefonoCli) {
+		this.telefonoCli = telefonoCli;
+	}
+
+	public String getEmailCli() {
+		return emailCli;
+	}
+
+	public void setEmailCli(String emailCli) {
+		this.emailCli = emailCli;
+	}
+
+	public Set<Reserva> getReserva() {
+		return reservas;
+	}
+
+	public void setReserva(Set<Reserva> reserva) {
+		this.reservas = reserva;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + documentoCli;
+		result = prime * result + ((emailCli == null) ? 0 : emailCli.hashCode());
+		result = prime * result + ((idCli == null) ? 0 : idCli.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (documentoCli != other.documentoCli)
+			return false;
+		if (emailCli == null) {
+			if (other.emailCli != null)
+				return false;
+		} else if (!emailCli.equals(other.emailCli))
+			return false;
+		if (idCli == null) {
+			if (other.idCli != null)
+				return false;
+		} else if (!idCli.equals(other.idCli))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [idCli=" + idCli + ", nombreCli=" + nombreCli + ", apellidoCli=" + apellidoCli
+				+ ", documentoCli=" + documentoCli + ", direccionCli=" + direccionCli + ", telefonoCli=" + telefonoCli
+				+ ", emailCli=" + emailCli + "]";
+	}
+
+	
+	
+}

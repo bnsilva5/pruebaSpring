c01d837b0799c597fbe71d331fb89089c84dc46e
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-04-08 17:14:33 -0500

    chore: se crea la interface reservaRepository

---
+++ src/main/java/co/edu/sena/eprueba/negocio/repository/ReservaRepository.java
@@ -0,0 +1,10 @@
+package co.edu.sena.eprueba.negocio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.sena.eprueba.modelo.Reserva;
+
+public interface ReservaRepository extends JpaRepository <Reserva, Long> {
+
+	
+}

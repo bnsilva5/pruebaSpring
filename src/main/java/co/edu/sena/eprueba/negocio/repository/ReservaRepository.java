d2152519acb4505f7d3cb2706ac413e333543d5d
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-04-09 15:41:24 -0500

    chore: se define consulta en la interface Repository

--- src/main/java/co/edu/sena/eprueba/negocio/repository/ReservaRepository.java
+++ src/main/java/co/edu/sena/eprueba/negocio/repository/ReservaRepository.java
@@ -1,10 +1,16 @@
 package co.edu.sena.eprueba.negocio.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import co.edu.sena.eprueba.modelo.Reserva;
 
 public interface ReservaRepository extends JpaRepository <Reserva, Long> {
 
-	
+	@Query("select r from Reserva r where r.fechaIngresoRes  =: fechaInicio and r.fechaSalidaRes =: fechaSalida")
+	public List<Reserva> find(@Param("fechaIngresoRes") Date fechaIngresoRes, @Param("fechaSalidaRes") Date fechaSalidaRes);
 }

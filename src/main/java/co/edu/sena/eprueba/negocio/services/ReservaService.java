88e03df40beaa7a862cee54b459ca09c031ba941
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-04-09 18:28:49 -0500

    chore: se crea e implementa la clase ReservaService

---
+++ src/main/java/co/edu/sena/eprueba/negocio/services/ReservaService.java
@@ -0,0 +1,61 @@
+package co.edu.sena.eprueba.negocio.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.sena.eprueba.modelo.Reserva;
+import co.edu.sena.eprueba.negocio.repository.ReservaRepository;
+
+@Service
+@Transactional(readOnly = true)
+public class ReservaService {
+	
+	private final ReservaRepository reservaRepository;
+	
+	public ReservaService(ReservaRepository reservaRepository) {
+		this.reservaRepository = reservaRepository;
+	}
+	
+	
+	/*
+	 * Metodo para crear una reserva
+	 */
+	@Transactional
+	public Reserva create(Reserva reserva) {
+		return this.reservaRepository.save(reserva);
+	}
+	
+	
+	/*
+	 * Metodo para actualizar una reserva
+	 */
+	@Transactional
+	public Reserva update(Reserva reserva) {
+		return this.reservaRepository.save(reserva);
+	}
+	
+	
+	/*
+	 * Metodo para eliminar una reserva
+	 */
+	@Transactional
+	public void delete(Reserva reserva) {
+		this.reservaRepository.delete(reserva);
+	}
+	
+	
+	/*
+	 * Metodo para consultar por IdReserva
+	 */
+	public Reserva findByIdRes(Long idRes) {
+		return this.reservaRepository.findByIdRes(idRes);
+	}
+	
+	
+	/*
+	 * Metodo para consultar por cantidad de personas que reservaron
+	 */
+	public Reserva findByCantidadPersonasRes(int cantidadPersonasRes) {
+		return this.reservaRepository.findByCantidadPersonasRes(cantidadPersonasRes);
+	}
+}

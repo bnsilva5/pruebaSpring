dff47ac99355a7a0bed942d82d51feb7defc7c80
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-04-09 15:39:12 -0500

    chore: se definen consultas en la interface Repository

--- src/main/java/co/edu/sena/eprueba/negocio/repository/ClienteRepository.java
+++ src/main/java/co/edu/sena/eprueba/negocio/repository/ClienteRepository.java
@@ -1,11 +1,15 @@
 package co.edu.sena.eprueba.negocio.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.edu.sena.eprueba.modelo.Cliente;
 
 public interface ClienteRepository extends JpaRepository <Cliente, Long> {
 	
+	List<Cliente> findByNombreCli(String nombreCli);
 	
+	public Cliente findByDocumentCli(String documentCli);
 
 }

d67085f09a07c1a38c3a1565e905c2085908a885
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-04-13 19:32:23 -0500

    chore: anotaciones para documentaciones de Api's

--- src/main/java/co/edu/sena/eprueba/vista/resource/ReservaResource.java
+++ src/main/java/co/edu/sena/eprueba/vista/resource/ReservaResource.java
@@ -9,9 +9,14 @@ import org.springframework.web.bind.annotation.*;
 import co.edu.sena.eprueba.modelo.Reserva;
 import co.edu.sena.eprueba.negocio.services.ReservaService;
 import co.edu.sena.eprueba.vista.resources.DTO.ReservaDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api/reserva")
+@Api(tags = "reserva")
 public class ReservaResource {
 
 	private final ReservaService reservaService;
@@ -22,6 +27,9 @@ public class ReservaResource {
 	
 	
 	@PostMapping
+	@ApiOperation(value = "Crear Reserva", notes = "Servicio para crear una reserva")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Reserva creada correctamente"),
+			@ApiResponse(code = 400, message = "Solicitud Invalida") })
 	public ResponseEntity<Reserva> createReserva(@RequestBody ReservaDTO reservaDto) {
 		Reserva reserva = new Reserva();
 		
@@ -34,6 +42,9 @@ public class ReservaResource {
 	
 	
 	@PutMapping("/{idRes}")
+	@ApiOperation(value = "Actualizar Reserva", notes = "Servicio para actualizar una reserva")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Reserva actualizada correctamente"),
+			@ApiResponse(code = 400, message = "Solicitud Invalida") })
 	public ResponseEntity<Reserva> updateReserva(@PathVariable("idRes") Long idRes, ReservaDTO reservaDto) {
 		Reserva reserva = this.reservaService.findByIdRes(idRes);
 		
@@ -52,6 +63,9 @@ public class ReservaResource {
 	
 	
 	@DeleteMapping
+	@ApiOperation(value = "Eliminar Reserva", notes = "Servicio para eliminar reserva")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "reserva eliminada correctamente"),
+			@ApiResponse(code = 400, message = "Solicitud Invalida") })
 	public void removeReserva(@PathVariable("idRes") Long idRes, ReservaDTO reservaDto) {
 		Reserva reserva = this.reservaService.findByIdRes(idRes);
 		
@@ -62,6 +76,9 @@ public class ReservaResource {
 	
 	
 	@GetMapping
+	@ApiOperation(value = "Listar reservas", notes = "Servicio para listar todas las reservas")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Reservas encontradas"),
+			@ApiResponse(code = 400, message = "Solicitud Invalida") })
 	public ResponseEntity<List<Reserva>> findAll() {
 		return ResponseEntity.ok(this.reservaService.findAll());
 	}
